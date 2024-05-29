58ff613cd38e4e723a0a773c982104b18713f4ca
Ambrose <ambymbayi45@gmail.com>
2024-05-29 21:27:13 +0300

    able to make network call

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,6 +15,16 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessages) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessages!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
