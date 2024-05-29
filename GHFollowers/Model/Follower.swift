3269fa757f0ea78a54d7471aa5b5de2557732d9d
Ambrose <ambymbayi45@gmail.com>
2024-05-29 18:35:30 +0300

    create model for users and followers

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Ambrose Mbayi on 29/05/2024.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
